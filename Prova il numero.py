4ec59170a4a68e44998a329f0ea5a6ce960a4a55
Peter Kocian <63299045+PeterKocian09@users.noreply.github.com>
2020-05-14 19:46:16 +0200

    Add files via upload

---
+++ Prova il numero.py
@@ -0,0 +1,10 @@
+import random
+x = raw_input("inserisci un numero")
+n = random.randint(1,100)
+while true:
+	if x == n:
+		print(hai scritto il numero giusto")
+	if x == n:
+		print(hai scritto il numero giusto")
+	if x  n:
+		print(hai scritto il numero giusto")
